ab769e7699358b18c919e5565e43397388b157a0
MrCookiefries <cookiefries12@gmail.com>
2021-12-19 18:18:56 -0600

    add comments

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const { portNum } = require("./config");
 
+// listen for requests
 app.listen(portNum, () => {
 	console.log(`Server running at http://localhost:${portNum}`);
 });
