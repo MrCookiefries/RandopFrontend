870c2ca2483ed5454edeeff2df2cb757f06479d2
MrCookiefries <cookiefries12@gmail.com>
2022-01-05 15:38:18 -0600

    add appropriate middleware to routes

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -2,43 +2,41 @@ const { Router } = require("express");
 const router = Router();
 
 const catchErrors = require("../middleware/catchErrors");
-const parseStringNums = require("../helpers/parseStringNums");
 const Cart = require("../models/Cart");
 const handleJsonValidator = require("../helpers/handleJsonValidator");
-const getManySchema = require("../jsonschemas/carts/getMany.json");
 const addNewSchema = require("../jsonschemas/carts/addNew.json");
 const updateSchema = require("../jsonschemas/carts/update.json");
+const { ensureUser, ensureOwnerOrAdmin } = require("../middleware/auth");
 
-router.get("/", catchErrors(async (req, res) => {
-	parseStringNums(req.query, ["limit", "offset"]);
-	handleJsonValidator(req.query, getManySchema);
-	const carts = await Cart.getMany(req.query);
-	return res.status(200).json({ carts });
-}));
+router.get("/", [ensureUser],
+	catchErrors(async (req, res) => {
+		const carts = await Cart.getByUser(res.user.id);
+		return res.status(200).json({ carts });
+	})
+);
 
-router.get("/:userId/:productId", catchErrors(async (req, res) => {
-	const { userId, productId } = req.params;
-	const cart = await Cart.getById(userId, productId);
-	return res.status(200).json({ cart });
-}));
+router.post("/", [ensureUser],
+	catchErrors(async (req, res) => {
+		req.body.userId = res.user.id;
+		handleJsonValidator(req.body, addNewSchema);
+		const cart = await Cart.addNew(req.body);
+		return res.status(201).json({ cart });
+	})
+);
 
-router.post("/", catchErrors(async (req, res) => {
-	handleJsonValidator(req.body, addNewSchema);
-	const cart = await Cart.addNew(req.body);
-	return res.status(201).json({ cart });
-}));
+router.patch("/:userId/:productId", [ensureUser, ensureOwnerOrAdmin],
+	catchErrors(async (req, res) => {
+		handleJsonValidator(req.body, updateSchema);
+		const cart = await Cart.updateById(req.params, req.body);
+		return res.status(200).json({ cart });
+	})
+);
 
-router.patch("/:userId/:productId", catchErrors(async (req, res) => {
-	handleJsonValidator(req.body, updateSchema);
-	const { userId, productId } = req.params;
-	const cart = await Cart.updateById(userId, productId, req.body);
-	return res.status(200).json({ cart });
-}));
-
-router.delete("/:userId/:productId", catchErrors(async (req, res) => {
-	const { userId, productId } = req.params;
-	await Cart.deleteById(userId, productId);
-	return res.status(204).send();
-}));
+router.delete("/:userId/:productId", [ensureUser, ensureOwnerOrAdmin],
+	catchErrors(async (req, res) => {
+		await Cart.deleteById(req.params);
+		return res.status(204).send();
+	})
+);
 
 module.exports = router;
