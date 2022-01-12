5fb0f7ffa6747c7fd4dde30d4b213ca0cddc9364
MrCookiefries <cookiefries12@gmail.com>
2022-01-12 17:41:58 -0600

    add start of local storage token helper

--- frontend/src/helpers/token.js
+++ frontend/src/helpers/token.js
@@ -1,11 +1,20 @@
-import Api from "./api";
+import Api from "./api.js";
+console.log(Api);
 
 export const getSavedToken = () => {
 	const token = window.localStorage.getItem("token");
+	console.log(Api);
+	// Api.token = token || undefined;
 	return token || undefined;
 };
 
 export const saveToken = token => {
 	window.localStorage.setItem("token", token);
+	console.log(Api);
 	Api.token = token || undefined;
+};
+
+export const removeToken = () => {
+	window.localStorage.removeItem("token");
+	Api.token = undefined;
 };
