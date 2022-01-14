fe3cd8054411b31e891f9f7727b616252e779a03
MrCookiefries <cookiefries12@gmail.com>
2022-01-14 15:19:33 -0600

    update schema for better carts & orders to be made

--- backend/schema/schema.sql
+++ backend/schema/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE users (
 	id SERIAL,
-	email VARCHAR(40) NOT NULL,
-	name VARCHAR(20) NOT NULL,
+	stripe_id TEXT,
+	email VARCHAR(254) NOT NULL,
+	name VARCHAR(70) NOT NULL,
 	password TEXT NOT NULL,
 	is_admin BOOLEAN NOT NULL DEFAULT FALSE,
 
@@ -28,15 +29,29 @@ CREATE TABLE products (
 	PRIMARY KEY (id)
 );
 
--- join table of products & users
+-- users can have many carts
 CREATE TABLE carts (
-	user_id INTEGER,
-	product_id TEXT,
-	quantity INTEGER NOT NULL DEFAULT 1,
+	id SERIAL,
+	user_id INTEGER NOT NULL,
 
 	CONSTRAINT fk_user
 		FOREIGN KEY (user_id)
 			REFERENCES users
+			ON DELETE CASCADE
+	,
+	
+	PRIMARY KEY (id)
+);
+
+-- join a product to a cart
+CREATE TABLE carts_products (
+	cart_id INTEGER NOT NULL,
+	product_id TEXT NOT NULL,
+	quantity INTEGER NOT NULL DEFAULT 1,
+
+	CONSTRAINT fk_cart
+		FOREIGN KEY (cart_id)
+			REFERENCES carts
 			ON DELETE CASCADE
 	,
 	CONSTRAINT fk_product
@@ -48,5 +63,43 @@ CREATE TABLE carts (
 		quantity > 0
 	),
 
-	PRIMARY KEY (user_id, product_id)
+	PRIMARY KEY (cart_id, product_id)
+);
+
+-- users can have many orders
+CREATE TABLE orders (
+	id SERIAL,
+	user_id INTEGER NOT NULL,
+	checkout_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
+
+	CONSTRAINT fk_user
+		FOREIGN KEY (user_id)
+			REFERENCES users
+			ON DELETE CASCADE
+	,
+	
+	PRIMARY KEY (id)
+);
+
+-- join a product to an order
+CREATE TABLE carts_products (
+	order_id INTEGER NOT NULL,
+	product_id TEXT NOT NULL,
+	quantity INTEGER NOT NULL DEFAULT 1,
+
+	CONSTRAINT fk_order
+		FOREIGN KEY (order_id)
+			REFERENCES orders
+			ON DELETE CASCADE
+	,
+	CONSTRAINT fk_product
+		FOREIGN KEY (product_id)
+			REFERENCES products
+			ON DELETE CASCADE
+	,
+	CONSTRAINT valid_quantity CHECK (
+		quantity > 0
+	),
+
+	PRIMARY KEY (order_id, product_id)
 );
