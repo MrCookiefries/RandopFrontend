69fdc1b8613d3db60609d5c8355d81bb9352a279
MrCookiefries <cookiefries12@gmail.com>
2022-01-14 22:34:49 -0600

    fix sequences to be restarted instead of started

--- backend/schema/testSequences.sql
+++ backend/schema/testSequences.sql
@@ -1,4 +1,4 @@
 -- allocate first 10 for manual testing
-ALTER SEQUENCE users_id_seq START WITH 11;
-ALTER SEQUENCE carts_id_seq START WITH 11;
-ALTER SEQUENCE orders_id_seq START WITH 11;
+ALTER SEQUENCE users_id_seq RESTART WITH 11;
+ALTER SEQUENCE carts_id_seq RESTART WITH 11;
+ALTER SEQUENCE orders_id_seq RESTART WITH 11;
