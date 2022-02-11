6a5259f3e096e35a87780118d6d4659d55510d36
MrCookiefries <cookiefries12@gmail.com>
2022-02-10 18:39:43 -0600

    fix redux devtools causing page to not load on mobile

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootReducer";
 import thunk from "redux-thunk";
 
-const store = createStore(
+const store = process.env.NODE_ENV !== "production" ? createStore(
 	rootReducer,
 	compose(
 		applyMiddleware(thunk),
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
-);
+): createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 export default store;
